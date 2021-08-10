d515b30d63e7bd17685eb5680244059a87a05e29
devCraf <caimgelus@gmail.com>
2021-08-09 19:34:26 -0500

    Add files via upload

---
+++ 02-vatiables-y-tipos/variables.py
@@ -0,0 +1,15 @@
+
+texto = "Master en Python"
+texto2 = "Con VCR"
+
+print(texto2)
+
+#------------------------ Condadenación
+
+nombre = "Ferney"
+apellido = "Calderon"
+mail = "devcraf@gmail.com"
+
+print(f"My name is {nombre} {apellido} and you can read on {mail}")
+
+
